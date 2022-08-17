7445c36cb4af2d6f27a66259f1c18313018e730d
Jecidhz18 <jecidhz18@hotmail.com>
2022-08-16 23:26:15 -0600

    refactor-page-administracion-primer-ingreso 2

---
+++ public/functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.menu-ul li:has(ul)').click(function(e){
+        e.preventDefault();
+
+        if($(this).hasClass('activado')){
+            
+       }else {
+           $('.menu-ul li ul').slideUp();
+           $('.menu-ul li').removeClass('activado');
+           $(this).addClass('activado');
+           $(this).children('ul').slideDown();
+       }
+    });
+});
